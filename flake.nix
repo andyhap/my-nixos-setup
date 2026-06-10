f9d16c10a4bde862cb6aab40f8afea42e4fbe8ed
Andy Hikmal <andyhikmalparakkasi@gmail.com>
2026-06-10 19:32:27 +0800

    lock yarara flake to nixos-26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Flake andyh";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     
     # Home Manager
     home-manager = {
